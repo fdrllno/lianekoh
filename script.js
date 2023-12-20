2c39260603f1497664688f79b2239e422052ca49
fdrllno <87590908+fdrllno@users.noreply.github.com>
2023-12-21 00:23:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const backgroundMusic = document.getElementById('background_music.mp3');
+
+backgroundMusic.src = 'background_music.mp3';
+
+backgroundMusic.volume = 0.5;
+
+backgroundMusic.play('background_music.mp3');
